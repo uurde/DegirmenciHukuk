4dc76456f5152080cffa2a3f5866aee280c6d0a7
Uğur Değirmenci <ugur@ugurdegirmenci.net>
2019-12-30 17:20:26 +0300

    .

--- DH.DataAccess/Context.cs
+++ DH.DataAccess/Context.cs
@@ -6,6 +6,7 @@ namespace DH.DataAccess
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //TODO: gitignore
             //NetDirekt Production SQL
             optionsBuilder.UseSqlServer(@"Server = 77.223.142.42; Initial Catalog = UgurDegirmenci; Persist Security Info = False; User ID = uurde; Password =0734ypkEUZ87; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = True; Connection Timeout = 30;");
         }
